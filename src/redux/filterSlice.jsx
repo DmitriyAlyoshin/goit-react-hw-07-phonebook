a5a5f69af5d54e08ac679aaa7136e4aa1f5abb5c
Dmitriy Alyoshin <valerich1715@gmail.com>
2023-07-13 20:35:37 +0200

    Start_ver_3

--- src/redux/filterSlice.jsx
+++ src/redux/filterSlice.jsx
@@ -5,19 +5,9 @@ export const filterSlice = createSlice({
   initialState: '',
 
   reducers: {
-    // setContactFilter: (state, action) => action.payload,
-    setContactFilter: (_, { payload }) => payload,
+     setContactFilter: (_, { payload }) => payload,
   },
 });
-
-// export const filterReducer = (state = initialState, { type, payload }) => {
-//   switch (type) {
-//     case SET_FILTER:
-//       return payload;
-//     default:
-//       return state;
-//   }
-// };
 
 export const { setContactFilter } = filterSlice.actions;
 export const filterReducer = filterSlice.reducer;
